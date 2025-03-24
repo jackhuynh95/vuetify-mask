c9509ac55421c57f05c0595ed5eb613986f23ed8
Jack Huynh <truong.huynh@nois.vn>
2025-03-24 11:41:32 +0700

    Migrate Vue3 (without maska)(the rest ...)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire">
     <v-card-title>
       <v-toolbar dark class="elevation-2" color="cyan">
-        <v-toolbar-title>vuetify-mask</v-toolbar-title>
+        <v-toolbar-title>vuetify-mask-next</v-toolbar-title>
       </v-toolbar>
     </v-card-title>
     <v-card-text>
@@ -20,7 +20,7 @@
           <v-col cols="4" sm="4" md="4">
             <v-text-field-integer />
           </v-col>
-          <!-- <v-col cols="4" sm="4" md="4">
+          <v-col cols="4" sm="4" md="4">
             <v-text-field-creditcard />
           </v-col>
           <v-col cols="4" sm="4" md="4">
@@ -52,7 +52,7 @@
           </v-col>
           <v-col cols="4" sm="4" md="4">
             <v-text-field-yearmonthdayday />
-          </v-col> -->
+          </v-col>
         </v-row>
       </v-container>
     </v-card-text>
@@ -66,17 +66,17 @@ import testMoney from "./TestMoney.vue";
 import testNegativeMoney from "./TestNegativeMoney.vue";
 import testPercent from "./TestPercent.vue";
 import testInteger from "./TestInteger.vue";
-// import testDateTime from "./TestDateTime.vue";
-// import testDateTimePicker from "./TestDateTimePicker.vue";
-// import testCreditCard from "./TestCreditCard.vue";
-// import testPhoneNumber from "./TestPhoneNumber.vue";
-// import testSimpleMask from "./TestSimpleMask.vue";
-// import testCpf from "./TestCpf.vue";
-// import testCnpj from "./TestCnpj.vue";
-// import testCep from "./TestCep.vue";
-// import testFileBase64 from "./TestFileBase64.vue";
-// import testDotNumber from "./TestDotNumber.vue";
-// import testYearMonthDayDay from "./TestYearMonthDayDay.vue";
+import testDateTime from "./TestDateTime.vue";
+import testDateTimePicker from "./TestDateTimePicker.vue";
+import testCreditCard from "./TestCreditCard.vue";
+import testPhoneNumber from "./TestPhoneNumber.vue";
+import testSimpleMask from "./TestSimpleMask.vue";
+import testCpf from "./TestCpf.vue";
+import testCnpj from "./TestCnpj.vue";
+import testCep from "./TestCep.vue";
+import testFileBase64 from "./TestFileBase64.vue";
+import testDotNumber from "./TestDotNumber.vue";
+import testYearMonthDayDay from "./TestYearMonthDayDay.vue";
 
 export default defineComponent({
   name: "app",
@@ -86,17 +86,17 @@ export default defineComponent({
     "v-text-field-negative-money": testNegativeMoney,
     "v-text-field-percent": testPercent,
     "v-text-field-integer": testInteger,
-    // "v-text-field-datetime": testDateTime,
-    // "v-text-field-creditcard": testCreditCard,
-    // "v-text-field-phonenumber": testPhoneNumber,
-    // "v-text-field-simplemask": testSimpleMask,
-    // "v-text-field-cpf": testCpf,
-    // "v-text-field-cnpj": testCnpj,
-    // "v-text-field-cep": testCep,
-    // "v-text-field-filebase64": testFileBase64,
-    // "v-text-field-dotnumber": testDotNumber,
-    // "v-text-field-datetimepicker": testDateTimePicker,
-    // "v-text-field-yearmonthdayday": testYearMonthDayDay,
+    "v-text-field-datetime": testDateTime,
+    "v-text-field-creditcard": testCreditCard,
+    "v-text-field-phonenumber": testPhoneNumber,
+    "v-text-field-simplemask": testSimpleMask,
+    "v-text-field-cpf": testCpf,
+    "v-text-field-cnpj": testCnpj,
+    "v-text-field-cep": testCep,
+    "v-text-field-filebase64": testFileBase64,
+    "v-text-field-dotnumber": testDotNumber,
+    "v-text-field-datetimepicker": testDateTimePicker,
+    "v-text-field-yearmonthdayday": testYearMonthDayDay,
   },
 });
 </script>
